54d450d95332a8a95d90045ef6f888e49f4be05a
McKinley Forster <McKinleyForster@gmail.com>
2022-08-07 07:25:39 -0400

    upd

--- Enemy Movement Patterns/script_enemy4.js
+++ Enemy Movement Patterns/script_enemy4.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 100;
+const numberOfEnemies = 10;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -11,24 +11,30 @@ let gameFrame = 0;
 class Enemy {
     constructor(){
         this.image = new Image();
-        this.image.src = 'enemy2.png';
+        this.image.src = 'enemy4.png';
         this.speed = Math.random() * 1 + 1;
-        this.spriteWidth = 266;
-        this.spriteHeight = 188;
+        this.spriteWidth = 213;
+        this.spriteHeight = 213;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
+        this.newX = Math.random() * canvas.width;
+        this.newY = Math.random() * canvas.height;
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.angle = 0;
-        this.angleSpeed = Math.random() * 0.2;
-        this.curve = Math.random() * 7;
+        this.interval = Math.floor(Math.random() * 200 + 50);
     }
     update(){
-        this.x -= this.speed;
-        this.y += this.curve * Math.sin(this.angle);
-        this.angle += this.angleSpeed;
+        if (gameFrame % this.interval === 0){
+            this.newX = Math.random() * (canvas.width - this.width);
+            this.newY = Math.random() * (canvas.height - this.height);
+        }
+        let dx = this.x - this.newX;
+        let dy = this.y - this.newY;
+        this.x -= dx/70;
+        this.y -= dy/70;
+
         if(this.x + this.width < 0) this.x = canvas.width
         // animate sprites
         if (gameFrame % this.flapSpeed === 0){
